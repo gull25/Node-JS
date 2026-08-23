61aa671cb66c2e955ddaee757a64f54b4c485350
Gul Rahman <gulr83447@gmail.com>
2026-08-23 12:45:37 +0500

    Making chnages in the logic

--- server.js
+++ server.js
@@ -74,17 +74,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const User = require("./models/User");
-require("dotenv").config(); // Load variables from .env
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 // Middleware
 app.use(express.json());
 
 // MongoDB Connection
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect("mongodb://127.0.0.1:27017/myDatabase")
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
